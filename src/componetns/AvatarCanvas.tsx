d49bab46c5d39fb24de3a1bc0380997e96160e0a
Towa Yokoyama <soccertowa0923@outlook.com>
2025-08-17 15:59:07 +0900

    it is my limited

--- src/componetns/AvatarCanvas.tsx
+++ src/componetns/AvatarCanvas.tsx
@@ -7,7 +7,7 @@ import { useGLTF, Image } from "@react-three/drei";
 
 const AVATAR_PATH = "/mymodel.glb";
 const BACKGROUND_PATH = "/background.png";
-
+const SUIT_PATH= "lalala.jpg"
 const Model = () => {
   const { scene } = useGLTF(AVATAR_PATH);
   
@@ -15,8 +15,8 @@ const Model = () => {
   return (
     <primitive
       object={scene}
-      position={[-1, 0.5, 1.2]}   // 左右、上下、前後
-      scale={0.4}             // 全体の大きさ
+      position={[-1.5, 0.6, 1]}   // 左右、上下、前後
+      scale={0.6}             // 全体の大きさ
     />
   );
 };
@@ -25,9 +25,17 @@ const SceneBackground = () => {
   return <Image
     url={BACKGROUND_PATH}
     position={[0, 0.5, -3]} // 背景の位置を少し上に調整
-    scale={7}             // 背景の大きさを少し調整
+    scale={8}             // 背景の大きさを少し調整
   />;
 };
+
+const SuitImage = () => {
+    return <Image 
+    url={SUIT_PATH}
+    position={[0,-0.8 , 0]} // X(左右), Y(上下), Z(前後)
+    scale={[1.54,0.6]}//横、縦 
+    />;
+}
 
 export const AvatarCanvas = () => {
   return (
@@ -39,11 +47,12 @@ export const AvatarCanvas = () => {
       <ambientLight intensity={1.5} />
       <directionalLight position={[3, 5, 2]} intensity={2} />
       
-      {/* ★★★ デバッグ用の補助線を追加 ★★★ */}
-      <axesHelper args={[5]} /> {/* X(赤), Y(緑), Z(青)の軸を表示 */}
-      <gridHelper />            {/* 地面にグリッドを表示 */}
+      {/* ★★★ デバッグ用の補助線を追加 ★★★ *
+      <axesHelper args={[5]} /> * X(赤), Y(緑), Z(青)のを表示 
+      <gridHelper />            地面グリッド */}
 
       <Model />
+      <SuitImage />
     </Canvas>
   );
 };
