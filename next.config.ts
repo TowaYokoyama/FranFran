6103f0b2fc218a0d1d7753a0a4b0fd6e9db7aa62
Towa Yokoyama <soccertowa0923@outlook.com>
2025-08-27 14:00:35 +0900

    Build: Disable ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:'standalone'
+  output:'standalone',
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
